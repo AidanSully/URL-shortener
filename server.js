fb6fb5c7f56709d8e4606780d7f8a62aae7dd12f
aidan s <aidansully@github.com>
2020-10-05 10:50:15 +0200

    set up basic api with endpoint

---
+++ server.js
@@ -0,0 +1,16 @@
+// Require
+const express = require("express");
+
+// Init
+const app = express();
+
+// Path
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Port
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
